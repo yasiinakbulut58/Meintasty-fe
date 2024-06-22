58c2ac808401f44bdef9d98dead8107f4305cb6d
yasiinakbulut58 <yasiinakbulut58@gmail.com>
2024-06-22 15:00:18 +0300

    fix: img src

--- src/data/footerData.tsx
+++ src/data/footerData.tsx
@@ -1,14 +1,14 @@
 export const news = [
   {
     id: 1,
-    img: '/assets/images/cab/blog-footer/1.jpg',
+    img: '/assets/images/restaurant/cat/1.jpg',
     title: 'recent news',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 00s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
   },
   {
     id: 2,
-    img: '/assets/images/cab/blog-footer/2.jpg',
+    img: '/assets/images/restaurant/cat/2.jpg',
     title: 'recent news',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 00s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
