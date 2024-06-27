9852b3751d188dd50766f93156651fb60c424f30
yasiinakbulut58 <yasiinakbulut58@gmail.com>
2024-06-27 20:27:05 +0300

    refactor: not-found

--- src/app/[lng]/not-found.tsx
+++ src/app/[lng]/not-found.tsx
@@ -1,14 +1,7 @@
-import { FC } from 'react';
-// import "@/public/assets/scss/color1.scss";
-import CustomLayout from '@/layouts/layout';
 import Error404Content from '@/components/others/404/page';
 
 const Error404 = () => {
-  return (
-    <CustomLayout title="overlay-black" userBgClass="user user-light">
-      <Error404Content />
-    </CustomLayout>
-  );
+  return <Error404Content />;
 };
 
 export default Error404;
