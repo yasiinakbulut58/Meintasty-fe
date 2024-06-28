460be212e59d64e2c61fbd082e9d991c409de0ae
yasiinakbulut58 <yasiinakbulut58@gmail.com>
2024-06-28 13:29:18 +0300

    fix: iis server

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
 const { createServer } = require('http');
-const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-
-const port = process.env.PORT || 3002; // Change the port to the port that your IIS is running on. Default its 80 and 3000 if you are using it for developing.
-const hostname = 'localhost';
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
-
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query);
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('internal server error');
-    }
-  })
-    .once('error', (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, async () => {
-      console.log(`> Ready on http://localhost:${port}`);
-    });
+  createServer((req, res) => {
+    // Custom routing if necessary
+    handle(req, res);
+  }).listen(process.env.PORT || 3000, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
+  });
 });
